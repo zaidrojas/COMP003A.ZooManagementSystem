d44c88927162fc8eed08257e98ce1659b142a6d6
zaidrojas <zaidnemesis83@outlook.com>
2025-02-18 12:26:07 -0800

    Add project files.

---
+++ COMP003A.ZooManagementSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿// Author: [Zaid Rojas]
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: Zoo management system demonstrating inheritance, abstraction, polymorphism, and method overloading in C#
+
+namespace COMP003A.ZooManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
