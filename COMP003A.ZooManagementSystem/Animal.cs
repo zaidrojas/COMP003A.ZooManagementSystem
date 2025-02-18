b17ce6d16db381da5f58fc91a804f1b002f97a84
zaidrojas <zaidnemesis83@outlook.com>
2025-02-18 13:26:27 -0800

    Implimented the abstract animal class

---
+++ COMP003A.ZooManagementSystem/Animal.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.ZooManagementSystem
+{
+    abstract class Animal
+    {
+        // Fields
+        private string _name;
+        private string _species;
+
+        // Public properties for fields
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty or whitespace.");
+                }
+                _name = value;
+            }
+        }
+
+        public string Species
+        {
+            get { return _species; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Species cannot be empty or whitespace.");
+                }
+                _species = value;
+            }
+        }
+
+        // Constructor
+        public Animal (string name, string species)
+        {
+            _name = name;
+            _species = species;
+
+        }
+
+        // Method to be overwritten
+        public abstract MakeSound();
+
+    }
+}
